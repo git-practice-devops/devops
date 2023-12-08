32f7a27e91ea7c26e0d69b84f3978d7849583d69
ashok <jayaprakash.career292@gmail.com>
2023-12-08 16:21:59 +0530

    commeting code in local repo

--- dev.sh
+++ dev.sh
@@ -1,2 +1,8 @@
- devlopment of code
+ devlopment code 
+ # include 
+ int a=10;
+ int b=20;
+ int c=30;
+ print ln ("hello world" )
+
 
---
+++ git.sh
@@ -0,0 +1,3 @@
+ hello world 
+ we are hear to develop code 
+
